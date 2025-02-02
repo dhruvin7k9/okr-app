28192eb3c756d72a8a0f6cada7eeea3a38c4ac98
dhruvin7k9 <dhruvink79@gmail.com>
2025-02-02 18:53:50 +0530

    update: removed databse service from objectives module

--- server-nest/src/objectives/objectives.module.ts
+++ server-nest/src/objectives/objectives.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ObjectivesController } from './objectives.controller';
 import { ObjectivesService } from './objectives.service';
-import {DatabaseService} from "../database/database.service";
 
 @Module({
   controllers: [ObjectivesController],
-  providers: [ObjectivesService, DatabaseService]
+  providers: [ObjectivesService]
 })
 export class ObjectivesModule {}
