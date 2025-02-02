9c33748219db862e3709c5dd50d6a67865eb304f
dhruvin7k9 <dhruvink79@gmail.com>
2025-02-02 17:41:35 +0530

    add: prisma service

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ObjectivesModule } from './objectives/objectives.module';
 import { DatabaseModule } from './database/database.module';
 import { KeyResultsModule } from './key-results/key-results.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ObjectivesModule, DatabaseModule, KeyResultsModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
