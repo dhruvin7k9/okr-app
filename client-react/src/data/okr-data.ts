d11d9b3292b6c1001b0155fc70eab174a49129a4
dhruvin7k9 <dhruvink79@gmail.com>
2025-02-05 00:12:53 +0530

    change: logic of helper functions

--- client-react/src/data/okr-data.ts
+++ client-react/src/data/okr-data.ts
@@ -1,48 +1,105 @@
-import {ObjectiveDisplayType, ObjectiveType} from "../types/okr-types";
+import {KeyResultInsertType, ObjectiveInsertType, ObjectiveType} from "../types/okr-types";
 
-async function insertOKRObjective (newOKR: ObjectiveDisplayType): Promise<void> {
-    const responseData = await fetch("http://localhost:3000/objectives", {
+const URL = "http://localhost:3000";
+
+async function getOKRObjectives(): Promise<ObjectiveType[]> {
+    const responseData = await fetch(`${URL}/objectives`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "GET"
+    });
+    return await responseData.json();
+}
+
+async function insertOKRObjective(newOKR: ObjectiveInsertType): Promise<void> {
+    const responseObjectiveData = await fetch(`${URL}/objectives`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
         method: "POST",
-        body: JSON.stringify(newOKR)
+        body: JSON.stringify({title: newOKR.title})
+    });
+    
+    const objective = await responseObjectiveData.json();
+    let keyResults = [];
+    
+    for(let i = 0; i < newOKR.keyResults.length; i++) {
+        keyResults.push({...newOKR.keyResults[i], objectiveId : objective.id});
+    }
+    
+    const responseKeyResultsData = await fetch(`${URL}/key-results/many`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(keyResults)
+    });
+    console.log(responseKeyResultsData);
+    return;
+}
+
+async function updateOKRKeyResult(keyResultId: string | number, updatedKeyResult: KeyResultInsertType) {
+    const responseData = await fetch(`${URL}/key-results/${keyResultId}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "PUT",
+        body: JSON.stringify(updatedKeyResult)
     });
     console.log(responseData);
     return;
 }
 
-async function getOKRObjectives (): Promise<ObjectiveType[]>  {
-    const responseData = await fetch("http://localhost:3000/objectives");
-    return await responseData.json();
-}
-
-async function updateOKRObjective (objectiveID: string, objective: ObjectiveType | undefined): Promise<void> {
-    const responseData = await fetch(`http://localhost:3000/objectives/${objectiveID}`, {
+async function updateOKRObjective(objectiveID: string | number | undefined, updatedObjectiveTitle: string): Promise<void> {
+    const responseData = await fetch(`${URL}/objectives/${objectiveID}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
         method: "PUT",
-        body: JSON.stringify(objective)
+        body: JSON.stringify({title : updatedObjectiveTitle})
     });
     console.log(`updated : ${responseData}`);
 }
 
-async function addOKRKeyResult (objectiveID: string, objective: ObjectiveType): Promise<void> {
-    const responseData = await fetch(`http://localhost:3000/objectives/${objectiveID}`, {
-        method: "PATCH",
-        body: JSON.stringify(objective)
+async function addOKRKeyResult(objectiveID: string | number | undefined, keyResult: KeyResultInsertType): Promise<void> {
+    const keyResultWithObjectiveId = {...keyResult, objectiveId : objectiveID!};
+    const responseData = await fetch(`${URL}/key-results`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(keyResultWithObjectiveId)
     });
     console.log(`updated objective with added key result: ${responseData}`);
 }
 
-async function deleteOKRObjective (objectiveID: string): Promise<void> {
-    const responseData = await fetch(`http://localhost:3000/objectives/${objectiveID}`, {
+async function deleteOKRObjective(objectiveID: string | number): Promise<void> {
+    const responseData = await fetch(`${URL}/objectives/${objectiveID}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
         method: "DELETE",
     });
     console.log(`deleted : ${responseData}`);
 }
 
-async function deleteOKRKeyResult (objectiveID : string, objective: ObjectiveType) {
-    const responseData = await fetch(`http://localhost:3000/objectives/${objectiveID}`, {
-        method: "PATCH",
-        body: JSON.stringify(objective)
+async function deleteOKRKeyResult(keyResultID: string | number) {
+    const responseData = await fetch(`${URL}/key-results/${keyResultID}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "DELETE",
     });
-    console.log(`deleted key result objective : ${responseData}`);
+    console.log(`deleted key result : ${responseData}`);
 }
 
-export {insertOKRObjective, getOKRObjectives, updateOKRObjective, deleteOKRObjective, deleteOKRKeyResult, addOKRKeyResult};
+export {
+    insertOKRObjective,
+    getOKRObjectives,
+    updateOKRObjective,
+    updateOKRKeyResult,
+    deleteOKRObjective,
+    deleteOKRKeyResult,
+    addOKRKeyResult
+};
